4d4207b2a3bfdd491047edce483ed717225c14c2
Amie Thao <amie.c.thao@gmail.com>
2019-02-12 09:39:23 -0600

    add JFrame and change color

--- Count.java
+++ Count.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 // extend class and implement actionlisteners
-public class Count extends Frame implements ActionListener {
+public class Count extends JFrame implements ActionListener {
 
   // setting variables with no values
   private Label labelInput;
@@ -27,7 +27,7 @@ public class Count extends Frame implements ActionListener {
 		// event listener
     textIn.addActionListener(this);
     
-    labelOut = new Label("The added sum is: ");
+    labelOut = new Label("The total sums added are: ");
     add(labelOut);  
 
     textOut = new TextField(10);
@@ -35,12 +35,11 @@ public class Count extends Frame implements ActionListener {
     add(textOut);
 
     setSize(350, 120); // set frame size defined width & height
-    setBackground(BLUE); // set frame background color to blue
+    setBackground(Color.RED); // set frame background color to blue
     setVisible(true); // set frame to display
 
     // when x btn is close the program stops running
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
   }
 
 
